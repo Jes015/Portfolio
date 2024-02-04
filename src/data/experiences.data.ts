4260121849e18a3f5386f5f8ba17af0d693f7818
Jes015 <jes.18.sin.15@gmail.com>
2024-02-04 15:32:40 -0500

    feat: update experience data (#98)
    close #98

--- src/data/experiences.data.ts
+++ src/data/experiences.data.ts
@@ -1,25 +1,5 @@
-import { CTechs, type TExperiencesArray } from "@src/models"
+import { type TExperiencesArray } from "@src/models"
 
 export const CExperiences: TExperiencesArray = [
-    {
-        company: "CulinaryAlchemy",
-        startDate: "2023-07",
-        endDate: "acutal",
-        companyURL: "https://github.com/CulinaryAlchemy/CulinaryAlchemy",
-        description: "In this project, I assumed full responsibility for the front-end development, meticulously crafting the user interface and ensuring a seamless user experience. Additionally, I implemented a CI/CD pipeline, including front-end testing, to streamline the development process, optimize code delivery, and maintain the highest standards of quality and efficiency.",
-        profesion: "Front-end developer",
-        techs: [
-            CTechs.React,
-            CTechs.Astro,
-            CTechs.playwright, 
-            CTechs.Typescript, 
-            CTechs.axios, 
-            CTechs.Zod, 
-            CTechs.swr,
-            CTechs.i18next,
-            CTechs.million,
-            CTechs.joyUI, 
-            CTechs.ReactHookForm
-        ]
-    }
+
 ]
